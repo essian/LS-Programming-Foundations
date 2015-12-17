d362f8c7fc5fe3024cf4531b8f963f8c1f526b8e
Jess Dennis <jessicampdennis@hotmail.com>
2015-12-17 13:29:03 +0000

    refactor to use current_player to determine control of gameplay

--- lesson_4/tictactoe.rb
+++ lesson_4/tictactoe.rb
@@ -6,8 +6,8 @@ COMPUTER_MARKER = 'O'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
                 [[1, 5, 9], [3, 5, 7]]
-FIRST_PLAYER = 'choose'
-VALID_CHOICES = %w(me computer)
+FIRST_PLAYER = 'c' # Valid options are choose, p for player or c for computer
+VALID_CHOICES = %w(p c)
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -47,6 +47,23 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def place_piece!(board, current_player)
+  if current_player == VALID_CHOICES[1]
+    computer_places_piece!(board)
+  elsif current_player == VALID_CHOICES[0]
+    player_places_piece!(board)
+  end
+end
+
+def alternate_player(current_player)
+  if current_player == VALID_CHOICES[0]
+    current_player = VALID_CHOICES[1]
+  elsif current_player == VALID_CHOICES[1]
+    current_player = VALID_CHOICES[0]
+  end
+  current_player
+end
+
 def player_places_piece!(brd)
   square = ''
   loop do
@@ -60,23 +77,22 @@ end
 
 def find_best_square(line, board, marker)
   if board.values_at(*line).count(marker) == 2
-    board.select{|k,v| line.include?(k) && v == INITIAL_MARKER}.keys.first
-  else
-    nil
+    board.select { |k, v| line.include?(k) && v == INITIAL_MARKER }.keys.first
+    # else
+    # nil
   end
 end
-
 
 def computer_places_piece!(brd)
   square = nil
 
   # offense
   WINNING_LINES.each do |line|
-  square = find_best_square(line, brd, COMPUTER_MARKER)
-  break if square
-end
+    square = find_best_square(line, brd, COMPUTER_MARKER)
+    break if square
+  end
 
-# defence
+  # defence
   if !square
     WINNING_LINES.each do |line|
       square = find_best_square(line, brd, PLAYER_MARKER)
@@ -84,15 +100,14 @@ end
     end
   end
 
-# pick 5 or just pick a square
-  if (!square  && empty_squares(brd).include?(5))
+  # pick 5 or just pick a square
+  if !square && empty_squares(brd).include?(5)
     square = 5
   end
 
   if !square
     square = empty_squares(brd).sample
   end
-
 
   brd[square] = COMPUTER_MARKER
 end
@@ -120,26 +135,25 @@ def update_score(scores, winner)
   scores[winner.downcase.to_sym] += 1
 end
 
-scores = {player: 0, computer: 0}
-
-if FIRST_PLAYER == 'choose'
-  prompt "Who starts? (Enter me or computer)"
-  choice = gets.chomp.downcase
-  if VALID_CHOICES.include?(choice)
-    FIRST_PLAYER == choice
-  else
-    prompt "That's not a valid choice. Please try again."
-  end
-end
+scores = { player: 0, computer: 0 }
 
 loop do
   board = initialize_board
-  computer_places_piece!(board) if FIRST_PLAYER == 'computer'
+  if FIRST_PLAYER == 'choose'
+    prompt "Who starts? (Enter p for player or c for computer})"
+    choice = gets.chomp.downcase
+    if VALID_CHOICES.include?(choice)
+      current_player = choice
+    else
+      prompt "That's not a valid choice. Please try again."
+    end
+  else current_player = FIRST_PLAYER
+  end
+
   loop do
     display_board(board)
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
-    computer_places_piece!(board)
+    place_piece!(board, current_player)
+    current_player = alternate_player(current_player)
     break if someone_won?(board) || board_full?(board)
   end
 
@@ -163,6 +177,6 @@ end
 champion = ''
 champion = 'Player' if scores[:player] == 5
 champion = 'Computer' if scores[:computer] == 5
-prompt ("#{champion} is the champion!") unless champion == ''
+prompt "#{champion} is the champion!" unless champion == ''
 
 prompt "Thanks for playing Tic Tac Toe. Goodbye!"
