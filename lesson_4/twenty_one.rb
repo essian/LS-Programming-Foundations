285080709ae8d55b5c7dfb94735e425c8b87f71f
Jess Dennis <jessicampdennis@hotmail.com>
2015-12-18 11:56:15 +0000

    includes first to 5 scoring

--- lesson_4/twenty_one.rb
+++ lesson_4/twenty_one.rb
@@ -1,67 +1,48 @@
- require 'pry'
+SUITS = ['H', 'D', 'S', 'C']
+VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
-CARDS = (2..10).to_a << 'J' << 'K' << 'K' << 'A'
-SUITS = %w(H D S C)
-
-def initialize_deck
-  SUITS.product(CARDS).shuffle
+def prompt(msg)
+  puts "=> #{msg}"
 end
 
-def prompt(message)
-  puts "=> #{message}"
+def initialize_deck
+  SUITS.product(VALUES).shuffle
 end
 
 def total(cards)
+  # cards = [['H', '3'], ['S', 'Q'], ... ]
   values = cards.map { |card| card[1] }
 
   sum = 0
   values.each do |value|
-    if value == 'A'
+    if value == "A"
       sum += 11
-    elsif ['J', 'Q', 'K'].include?(value)
+    elsif value.to_i == 0 # J, Q, K
       sum += 10
     else
       sum += value.to_i
     end
   end
 
-  values.count { |value| value == 'A' }.times do
+  # correct for Aces
+  values.count { |value| value == "A" }.times do
     sum -= 10 if sum > 21
   end
 
   sum
 end
 
-def busted?(cards)
-  total(cards) > 21
+def busted?(total)
+  total > 21
 end
 
-def display_results(dealer_cards, player_cards)
-  result = detect_result(dealer_cards, player_cards)
-
-  case result
-  when :player_busted
-    prompt "You busted! Dealer wins!"
-  when :dealer_busted
-    prompt "Dealer busted! You win!"
-  when :dealer
-    prompt "Dealer wins!"
-  when :player
-    prompt "Player wins!"
-  when :tie
-    prompt "It's a tie!"
-  end
-end
-
-def detect_result(dealer_cards, player_cards)
-  player_total = total(player_cards)
-  dealer_total = total(dealer_cards)
-
+# :tie, :dealer, :player, :dealer_busted, :player_busted
+def detect_result(dealer_total, player_total)
   if player_total > 21
     :player_busted
   elsif dealer_total > 21
     :dealer_busted
-  elsif player_total > dealer_total
+  elsif dealer_total < player_total
     :player
   elsif dealer_total > player_total
     :dealer
@@ -70,108 +51,139 @@ def detect_result(dealer_cards, player_cards)
   end
 end
 
+def display_cards(dealer_total, player_total, dealer_cards, player_cards)
+  puts "=============="
+  prompt "Dealer has #{dealer_cards}, for a total of: #{dealer_total}"
+  prompt "Player has #{player_cards}, for a total of: #{player_total}"
+  puts "=============="
+end
+
+def display_result(dealer_total, player_total)
+  result = detect_result(dealer_total, player_total)
+
+  case result
+  when :player_busted
+    prompt "You busted! Dealer wins!"
+  when :dealer_busted
+    prompt "Dealer busted! You win!"
+  when :player
+    prompt "You win!"
+  when :dealer
+    prompt "Dealer wins!"
+  when :tie
+    prompt "It's a tie!"
+  end
+end
+
+def display_score(scores)
+  prompt "Dealer score is #{scores[:dealer]}, player score is #{scores[:player]}"
+end
+
+def display_outcome(dealer_total, player_total, dealer_cards, player_cards, scores)
+  display_cards(dealer_total, player_total, dealer_cards, player_cards)
+  display_result(dealer_total, player_total)
+  display_score(scores)
+end
+
+def champion?(scores)
+  if scores[:dealer] == 5
+    return 'Dealer'
+  elsif scores[:player] == 5
+    return 'Player'
+  end
+end
+
 def play_again?
-  puts "-----------"
+  puts "-------------"
   prompt "Do you want to play again? (y or n)"
   answer = gets.chomp
   answer.downcase.start_with?('y')
 end
 
-def display_grand_totals(dealer_cards, player_cards, dealer_total, player_total)
-  puts "==========="
-  prompt "Dealer has #{dealer_cards}, for a total of #{dealer_total}"
-  prompt "Player has #{player_cards} for a total of #{player_total}"
-  puts "==========="
-end
-
-def adjust_score(dealer_cards, player_cards, scores)
-  case detect_result(dealer_cards, player_cards)
-  when :player_busted || :dealer
-    scores[:dealer_score] += 1
-  when :dealer_busted || :player_score
-    scores[:player_score] += 1
-  end
-end
-
+scores = { player: 0, dealer: 0 }
 
 loop do
   prompt "Welcome to Twenty-One!"
 
+  # initialize vars
   deck = initialize_deck
   player_cards = []
   dealer_cards = []
-  scores = {player_score: 0, dealer_score: 0}
 
+  # initial deal
   2.times do
     player_cards << deck.pop
     dealer_cards << deck.pop
   end
 
-  player_total = total(player_cards)
   dealer_total = total(dealer_cards)
+  player_total = total(player_cards)
 
   prompt "Dealer has #{dealer_cards[0]} and ?"
-  prompt "You have: #{player_cards[0]} and #{player_cards[1]} for a total of #{player_total}"
+  prompt "You have: #{player_cards[0]} and #{player_cards[1]}, for a total of #{total(player_cards)}."
 
+  # player turn
   loop do
     player_turn = nil
     loop do
       prompt "Would you like to (h)it or (s)tay?"
       player_turn = gets.chomp.downcase
       break if ['h', 's'].include?(player_turn)
-      prompt "Please enter 'h' or 's'"
+      prompt "Sorry, must enter 'h' or 's'."
     end
 
     if player_turn == 'h'
       player_cards << deck.pop
-      prompt "You chose to hit."
+      prompt "You chose to hit!"
+      prompt "Your cards are now: #{player_cards}"
       player_total = total(player_cards)
-      prompt "Your cards are now #{player_cards} and your total is #{player_total}"
+      prompt "Your total is now: #{player_total}"
     end
 
-    break if player_turn == 's' || busted?(player_cards)
+    break if player_turn == 's' || busted?(player_total)
   end
 
-  if busted?(player_cards)
-    display_grand_totals(dealer_cards, player_cards, dealer_total, player_total)
-    display_results(dealer_cards, player_cards)
-    binding.pry
-    adjust_score(dealer_cards, player_cards, scores)
-    puts scores
+  if busted?(player_total)
+    scores[:dealer] += 1
+    display_outcome(dealer_total, player_total, dealer_cards, player_cards, scores)
+    break if champion?(scores)
     play_again? ? next : break
   else
     prompt "You stayed at #{player_total}"
   end
 
   # dealer turn
-
   prompt "Dealer turn..."
 
   loop do
-    break if busted?(dealer_cards) || total(dealer_cards) > 17
+    break if busted?(dealer_total) || dealer_total >= 17
 
     prompt "Dealer hits!"
     dealer_cards << deck.pop
+    dealer_total = total(dealer_cards)
     prompt "Dealer's cards are now: #{dealer_cards}"
   end
 
-  dealer_total = total(dealer_cards)
-  if busted?(dealer_cards)
+  if busted?(dealer_total)
     prompt "Dealer total is now: #{dealer_total}"
-    display_grand_totals(dealer_cards, player_cards, dealer_total, player_total)
-    display_results(dealer_cards, player_cards)
-    adjust_score(dealer_cards, player_cards, scores)
-    puts scores
+    scores[:player] += 1
+    display_outcome(dealer_total, player_total, dealer_cards, player_cards, scores)
+    break if champion?(scores)
     play_again? ? next : break
   else
     prompt "Dealer stays at #{dealer_total}"
   end
 
-  display_grand_totals(dealer_cards, player_cards, dealer_total, player_total)
-  display_results(dealer_cards, player_cards)
-  adjust_score(dealer_cards, player_cards, scores)
+  # both player and dealer stays - compare cards and update score!
+  if detect_result(dealer_total, player_total) == :dealer
+    scores[:dealer] += 1
+  elsif detect_result(dealer_total, player_total) == :player
+    scores[:player] += 1
+  end
+  display_outcome(dealer_total, player_total, dealer_cards, player_cards, scores)
+  break if champion?(scores)
   break unless play_again?
-  
 end
 
-prompt "Thank you for playing twenty-one! Goodbye!"
+prompt "The champion is #{champion?(scores)}" if champion?(scores)
+prompt "Thank you for playing Twenty-One! Good bye!"
