43640f92deb9c6a178723bf3fe6386e52ba443a3
Jessica Dennis BJSS <jessica.dennis@bjss.com>
2016-01-18 17:29:34 +0000

    latest work

--- lesson_2/rock_paper_scissors2.rb
+++ lesson_2/rock_paper_scissors2.rb
@@ -1,5 +1,42 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
 def prompt(message)
-	puts "=> message"
+	puts "=> #{message}"
 end
 
-prompt "Welcome to rock paper scissors"
+def calculate_result(player_choice, computer_choice)
+	if player_choice == 'rock' && computer_choice == 'paper'
+		'Computer'
+	elsif player_choice == 'rock' && computer_choice == 'scissors'
+		'Player'
+	elsif player_choice == 'scissors' && computer_choice == 'paper'
+			'Player'
+	elsif player_choice == 'scissors' && computer_choice == 'rock'
+			'Computer'
+	elsif player_choice == 'paper' && computer_choice == 'scissors'
+			'Computer'
+	elsif player_choice == 'paper' && computer_choice == 'rock'
+			'Player'
+	else
+		'Nobody'
+	end
+end
+
+play_again = true
+
+while play_again
+	prompt "Welcome to rock paper scissors"
+	prompt "Please select rock paper or scissors"
+	player_choice = gets.chomp.downcase
+
+	computer_choice = VALID_CHOICES.sample
+
+	prompt "Computer chose #{computer_choice}"
+
+	winner = calculate_result(player_choice, computer_choice)
+	prompt "#{winner} wins"
+	prompt "Would you like to play again?"
+	play_again = false if gets.chomp.downcase == 'n'
+end
+
+prompt "Thanks for playing!"
