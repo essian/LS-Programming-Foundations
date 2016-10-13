4dcf7d57d315a74a76bbf13e50cbb49b49828f91
essian <jessicampdennis@hotmail.com>
2016-10-13 09:39:55 +0100

    moves error handling to of_codon method

--- challenges/Medium_1/protein_translation/protein_translation.rb
+++ challenges/Medium_1/protein_translation/protein_translation.rb
@@ -11,11 +11,11 @@ class Translation
              %w(UAA UAG UGA) => 'STOP' }.freeze
 
   def self.of_codon(codon_string)
-    CODONS.select { |key, _| key.include?(codon_string) }.values.first
+    raise InvalidCodonError if codon_string =~ /[^AUCG]/
+    CODONS.detect { |key, _| key.include?(codon_string) }.last
   end
 
   def self.of_rna(strand)
-    raise InvalidCodonError if strand =~ /[^AUCG]/
     strand.scan(/.{3}/).each_with_object([]) do |codon_string, answer|
       codon = of_codon(codon_string)
       return answer if codon == 'STOP'
