e58c049d5e464dab68c0ccd8a66e7bcbf041593b
essian <jessicampdennis@hotmail.com>
2016-10-19 13:42:26 +0100

    add todos app for 170

--- 120_Object_Oriented_Programming/practice2.rb
+++ 120_Object_Oriented_Programming/practice2.rb
@@ -1,57 +1,9 @@
-class Employee
-
-  def initialize(name, serial)
-    @name = name
-    @serial = serial
-  end
+def call_me(some_code)
+  name = "jessy"
+  some_code.call            # call will execute the "chunk of code" that gets passed in
 end
 
-class Exec < Employee
-  include Delegation
-  def initialize(name, serial)
-    super(name, serial)
-    @contract = 'Full Time'
-    @desk = 'corner'
-    @holiday = 20
-    @prefix = 'Exe'
-  end
-end
+name = "Robert"
+chunk_of_code = Proc.new {puts "hi #{name}"}
 
-class Mgr < Employee
-  include Delegation
-  def initialize(name, serial)
-    super(name, serial)
-    @contract = 'Full Time'
-    @desk = 'private'
-    @holiday = 14
-    @prefix = 'Mgr'
-  end
-end
-
-class Regular < Employee
-  include Delegation
-  def initialize(name, serial)
-    super(name, serial)
-    @contract = 'Full Time'
-    @desk = 'cubicle'
-    @holiday = 10
-    @prefix = nil
-  end
-end
-
-class PartTime < Employee
-  def initialize
-    super(name, serial)
-    @contract = 'Part Time'
-    @desk = 'hotdesk'
-    @holiday = 0
-    @prefix = nil
-  end
-end
-
-
-Module Delegation
-def delegate
-
-end
-end
+call_me(chunk_of_code)
